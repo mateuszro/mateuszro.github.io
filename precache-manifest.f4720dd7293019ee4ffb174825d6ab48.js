b0a378e9e6e9f622fee8212e59cb3b3e4373748d
mateuszro <mateuszr1994@gmail.com>
2019-10-23 16:16:18 +0200

    Add files

---
+++ precache-manifest.f4720dd7293019ee4ffb174825d6ab48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401a920302bb9012090ca0d01f54811a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96833a530c33cdcfcb4a",
+    "url": "/static/css/main.2057c7de.chunk.css"
+  },
+  {
+    "revision": "372965030dc339f3d835",
+    "url": "/static/js/2.5ac0f61b.chunk.js"
+  },
+  {
+    "revision": "96833a530c33cdcfcb4a",
+    "url": "/static/js/main.b110baff.chunk.js"
+  },
+  {
+    "revision": "1dd3736b1c02a196102b",
+    "url": "/static/js/runtime-main.33a82a6f.js"
+  }
+]);
